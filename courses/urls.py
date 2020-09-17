c0c318a11e28c85e6d99971515c5f3db7bd07b0f
Wen Jen <wjen1214@gmail.com>
2020-09-17 14:56:41 -0700

    add authorization

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,10 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('courses', views.CourseView)
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
